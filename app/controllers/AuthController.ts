97c27002cee320ce495a8e07d26614d10b2e1c06
ShariarSheikh <shariar.dev@gmail.com>
2023-09-27 18:24:29 +0600

    created category route

--- app/controllers/AuthController.ts
+++ app/controllers/AuthController.ts
@@ -1,10 +1,5 @@
 import AuthModel, { IAuth } from '../models/Auth.Model'
 
-interface LoginPropsData {
-  email: string
-  password: string
-}
-
 class AuthController {
   public async findUserWithEmail(email: string): Promise<IAuth | null> {
     return await AuthModel.findOne({ email })
