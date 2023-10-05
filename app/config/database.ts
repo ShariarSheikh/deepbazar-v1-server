3a33af60d8e461cd6bca798e75f1d18c72ffd086
ShariarSheikh <shariar.dev@gmail.com>
2023-10-05 18:23:41 +0600

    added new profile route

--- app/config/database.ts
+++ app/config/database.ts
@@ -1,6 +1,6 @@
 import mongoose, { ConnectOptions } from 'mongoose'
 import logger from '../core/Logger'
-import { databaseConfig, environment, localDbUrl } from './variables.config'
+import { databaseConfig, environment } from './variables.config'
 
 // DB variables
 const cloudDbUrl = `mongodb+srv://${databaseConfig.username}:${databaseConfig.password}@${databaseConfig.cluster}.ebdrpdu.mongodb.net/${databaseConfig.dbName}?retryWrites=true&w=majority`
@@ -8,7 +8,8 @@ const dbOptions: ConnectOptions = {
   autoIndex: true
 }
 
-const databaseUrl = environment === 'development' ? localDbUrl : cloudDbUrl
+// const databaseUrl = environment === 'development' ? localDbUrl : cloudDbUrl
+const databaseUrl = cloudDbUrl
 
 const connectDatabase = async () => {
   try {
